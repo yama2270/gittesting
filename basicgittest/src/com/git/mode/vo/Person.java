13215f6ff27fb06a33e14a777a7945ca15b22e91
pc <pc@DESKTOP-NELI316>
2021-04-14 10:45:25 +0900

    #01 Person vo추가함

---
+++ basicgittest/src/com/git/mode/vo/Person.java
@@ -0,0 +1,41 @@
+package com.git.mode.vo;
+
+public class Person {
+	
+	private String name;
+	private int age;
+	private String email;
+	
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+}
