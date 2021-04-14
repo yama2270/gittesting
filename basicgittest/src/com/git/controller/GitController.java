dcd800225092dcb0cedbff85896e62644715a991
pc <pc@DESKTOP-NELI316>
2021-04-14 10:15:29 +0900

    #03 기능추가(testPrint,calcPlus) -hy-

---
+++ basicgittest/src/com/git/controller/GitController.java
@@ -0,0 +1,13 @@
+package com.git.controller;
+
+public class GitController {
+	
+	public void testPrint() {
+		System.out.println("프린트하는것");
+	}
+	
+	public int calcPlus(int su1, int su2) {
+		return su1 + su2;
+	}
+	
+}
